eb3c05048883c94a007beb7100fa016f1a7062da
xiaojianli <376127890@qq.com>
2018-08-31 18:16:17 +0800

    2

---
+++ prd/html/files/统一认证平台/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(),R,_(S,[_(T,U,V,W,X,Y,n,Z,ba,Z,bb,bc,s,_(bd,_(be,bf,bg,bf)),P,_(),bh,_(),bi,[],bj,g)])),bk,_(),bl,_(bm,_(bn,bo)));}; 
+var b="url",c="统一认证平台.html",d="generationDate",e=new Date(1535704819074.46),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="7e6da78afc5f41d4a66cdb9542732a10",n="type",o="Axure:Page",p="name",q="统一认证平台",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="diagram",S="objects",T="id",U="c9fdf5960ebb4cd8a462bb6abd1d9659",V="label",W="",X="friendlyType",Y="组合",Z="layer",ba="styleType",bb="visible",bc=true,bd="location",be="x",bf=0,bg="y",bh="imageOverrides",bi="objs",bj="propagate",bk="masters",bl="objectPaths",bm="c9fdf5960ebb4cd8a462bb6abd1d9659",bn="scriptId",bo="u28";
+return _creator();
+})());
